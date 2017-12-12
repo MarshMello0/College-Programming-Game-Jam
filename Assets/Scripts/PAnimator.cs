ea0314b1ee806403a668cbb310e1a214048637c6
MarshMello <33008390+MarshMello0@users.noreply.github.com>
2017-12-12 19:56:33 +0100

    Build

---
+++ Assets/Scripts/PAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PAnimator : MonoBehaviour
+{
+    public GameManager manager;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _timer;
+    [SerializeField] private float _timerIncrease;
+
+    private void Start()
+    {
+        _animator.SetFloat("Speed",0);
+    }
+
+    private void Update()
+    {
+        if (manager.inGame)
+        {
+            if (_timer <= 7)
+            {
+                _animator.SetFloat("Speed",_timer);
+                _timer += _timerIncrease;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space) && manager.inGame)
+        {
+            _animator.Play("Jump");
+        }
+    }
+}
